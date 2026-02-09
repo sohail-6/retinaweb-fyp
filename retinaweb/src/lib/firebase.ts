e6b2638ec9ea2218be0988111068fd8275a4f10c
Hamza Mustafa Qureshi <57688975+hamzamq19@users.noreply.github.com>
2026-02-09 18:28:16 +0500

    Update firebase.ts

--- retinaweb/src/lib/firebase.ts
+++ retinaweb/src/lib/firebase.ts
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkn6DnpEr9pBfWcH9b-QFQwt1wvqlueV8",
-  authDomain: "retinaai-41045.firebaseapp.com",
-  projectId: "retinaai-41045",
-  storageBucket: "retinaai-41045.firebasestorage.app",
-  messagingSenderId: "455781539118",
-  appId: "1:455781539118:web:c25b4859c0d889d2442ebf",
-  measurementId: "G-7XBWHWQJ9L"
+  
 };
 
 // Initialize Firebase
